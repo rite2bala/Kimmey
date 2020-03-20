8aaa2d2520cfa18553b38b87a568aa99be3d7acd
Balakrishna <balakrishna.chandrasekaran@novartis.com>
2020-03-20 19:22:22 +0530

    Site changes only:

--- src/scenes/site/SiteContext.js
+++ src/scenes/site/SiteContext.js
@@ -15,14 +15,14 @@ const HCPProvider = (props) => {
         nameOfHCP2: 'Dr. ZZZ YYYY',
         hospitalPONumber: 'Hospital_PO100',
         hospitalContact: "+1-123-456-7890",
-        cityofHCP: "Jersey City",
+        cityofHCP: "Tampa, FL",
         countryofHCP: "USA",
 
         leukapheresisDate: '01/01/2020',
-        leukapheresisLocation: 'HCP, Jersey City',
+        leukapheresisLocation:  `John Hopkins All Childeren's hospital`,
         cryoPreservedLeukapheresisLocation: 'Cryoport Hub, NJ',
         reinfusionDate: '01/02/2020',
-        reinfusionLocation: 'HCP, Jersey City',
+        reinfusionLocation:  `John Hopkins All Childeren's hospital`,
     
         payerName: 'Optum Health Insurance',
         payerApprovalNumber: 'HN100'
